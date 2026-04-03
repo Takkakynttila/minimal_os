3f739ee331fafd82dcb80b2a29ebee30d27350d4
tatu <tatu.possi@gmail.com>
2026-04-03 16:00:16 +0300

    fixed flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+// Source - https://stackoverflow.com/a/67902310
+// Posted by phip1611, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-03, License - CC BY-SA 4.0
+
+// disable rust standard library
+#![no_std]
+// disables Rust runtime init,
+#![no_main]
+
+// see https://docs.rust-embedded.org/embedonomicon/smallest-no-std.html
+#![feature(lang_items)]
+
+// see https://docs.rust-embedded.org/embedonomicon/smallest-no-std.html
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+use core::panic::PanicInfo;
+use core::sync::atomic;
+use core::sync::atomic::Ordering;
+
+#[no_mangle]
+/// The name **must be** `_start`, otherwise the compiler throws away all code as unused. 
+/// The name can be changed by passing a different entry symbol as linker argument.
+fn _start() -> ! {
+    loop {}
 }
+
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        atomic::compiler_fence(Ordering::SeqCst);
+    }
+}
+
